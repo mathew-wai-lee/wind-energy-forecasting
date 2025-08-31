f2fedf6e34614f89803878f63a5d6cd7ae70ac16
Mathew Lee <mwlee9@gmail.com>
2025-08-31 15:24:42 -0500

    Working zeppelin notebooks and debugger.

--- src/main/scala/com/mathewlee/windenergyforecasting/Main.scala
+++ src/main/scala/com/mathewlee/windenergyforecasting/Main.scala
@@ -3,21 +3,17 @@ package com.mathewlee.windenergyforecasting
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 
-/**
-  * Use this when submitting the app to a cluster with spark-submit
-  * */
-object mainProd extends App{
+/** Use this when submitting the app to a cluster with spark-submit
+  */
+object mainProd extends App {
   // spark-submit command should supply all necessary config elements
   Runner.run(new SparkConf())
 }
 
-
-/**
-  * Use this to test the app locally, from sbt:
-  * sbt "run inputFile.txt outputFile.txt"
-  *  (+ select CountingLocalApp when prompted)
+/** Use this to test the app locally, from sbt: sbt "run inputFile.txt
+  * outputFile.txt" (+ select CountingLocalApp when prompted)
   */
-object mainLocal extends App{
+object mainLocal extends App {
   val conf = new SparkConf()
     .setMaster("local")
     .setAppName("test runner")
@@ -26,28 +22,34 @@ object mainLocal extends App{
 
 object Runner {
   def run(conf: SparkConf): Unit = {
-    val spark = SparkSession.builder()
+    val spark = SparkSession
+      .builder()
       .config(conf)
       .getOrCreate()
-    
+
     println("Spark Session created successfully.")
-    
-    val df = spark.read.csv("data/step1_original_merged_output.csv")
+
+    val df = spark.read
+      .option("header", "true")
+      .csv("data/Turbine_Data.csv")
     println("CSV Read successfully")
-    
+
     println("")
     println(df.show(5))
     println("")
-    
+
     println("")
-    println(df.columns)
+    println(df.printSchema())
     println("")
-    
+
     println("")
     println(s"There are: ${df.count()} rows")
     println("")
-  
+
+    println("")
+    println(df.describe().show(truncate = false))
+    println("")
+
     scala.io.StdIn.readLine()
   }
 }
-
